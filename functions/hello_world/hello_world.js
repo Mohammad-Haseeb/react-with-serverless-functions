8976683ca34b9ae8febd93bc5d5e9c177be69da4
Mohammad-Haseeb <mohammad.haseeb310@gmail.com>
2021-02-19 00:02:17 +0500

    learn

--- functions/hello_world/hello_world.js
+++ functions/hello_world/hello_world.js
@@ -1,10 +1,13 @@
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
 const handler = async (event) => {
+  
+
   try {
-    const subject = event.queryStringParameters.name || 'World'
+    const subject = JSON.parse(event.body).name || "No name" ;
+    
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
+      body: JSON.stringify({ message:   `${subject}` }),
       // // more keys you can return:
       // headers: { "headerName": "headerValue", ... },
       // isBase64Encoded: true,
